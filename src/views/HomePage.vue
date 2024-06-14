a40e33f394bab9f7377d3141bdb21fd5b1610135
Lucas Alex <joli.gp.29@gmail.com>
2024-06-14 04:21:28 -0300

    feat: initial page

---
+++ src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable-next-line vue/multi-word-component-names -->
+<template>
+  <div class="home">
+    <h1>Bem-vindo à Página Inicial</h1>
+    <p>Esta é a página inicial do meu aplicativo Vue com Vue Router!</p>
+  </div>
+</template>
+
+<style>
+.home {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
